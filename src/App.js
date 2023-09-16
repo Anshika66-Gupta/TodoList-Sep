d31b7e3a1bfc97609f3b813b6751fbd3f7b054e4
“Anshika <ansgupta.01@gmail.com>
2023-09-16 19:09:20 +0530

    Add items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
 import './App.css';
-
 import React from 'react'
+import ListItems from './ListItems';
 
 class App extends React.Component { 
   constructor(props) {
@@ -14,16 +13,49 @@ class App extends React.Component {
         key:''
       }
     }
+    this.handleInput = this.handleInput.bind(this); 
+    this.addItem = this.addItem.bind(this); 
+
+  }
+  
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key:Date.now()
+      }
+      
+    })
+  }
+  
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if (newItem.text !== ""){
+      const newItems = [...this.state.item, newItem];
+      this.setState({
+        item: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
   }
   render() {
     return(
       <div className='App'>
         <header>
-        <form id='to-do-form'>
-          <input type="text" placeholder='Enter Text' />
+        <form id='to-do-form' onSubmit={this.addItem}>
+            <input type="text" placeholder='Enter Text'
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
         <button type='submit'>Add</button>
         </form>
-      </header>
+        </header>
+        <ListItems items = {this.state.items}></ListItems>
       </div>
     )
   }
